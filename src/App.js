65627b6007e25f4af77022437a1c358ec122f095
bee60990 <155386705+bee60990@users.noreply.github.com>
2025-02-10 13:12:33 +0100

    Added footer infos

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
     <div className="App">
       <h1> Weather Engine</h1>
       <Search />
+      <p>
+        This code has been coded by Julia and can be checked on this{" "}
+        <a href="https://github.com/bee60990/weather_engine">
+          GitHub Repository
+        </a>
+      </p>
     </div>
   );
 }
